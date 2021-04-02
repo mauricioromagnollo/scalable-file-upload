9409f0313896eb83791bf1b12ede74043158179e
x0n4d0 <romagnolimauricio@gmail.com>
2021-04-02 15:38:44 -0300

    chore: rename path

---
+++ packages/server/src/index.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const socketIo = require('socket.io');
+const Routes = require('./routes');
+
+const PORT = 3000;
+
+const handler = function (request, response) {
+  const defaultRoute = async (request, response) => response.end('Hello');
+
+  const routes = new Routes(io);
+  const chosen = routes[request.method.toLowerCase()] || defaultRoute;
+  return chosen.apply(routes, [request, response]);
+}
+
+const server = http.createServer(handler);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    credentials: false
+  }
+});
+
+io.on("connection", (socket) => console.log('[!] Someone Connected!', socket.id));
+io.emit('file-uploaded', );
+
+const startServer = () => {
+  const { address, port } = server.address();
+  console.log(`[*] Server running at http://${address}:${port}`);
+}
+
+server.listen(PORT, startServer);
