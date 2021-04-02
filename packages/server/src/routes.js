3c07cb22f05b5513f0249efba53fce0c81dee647
x0n4d0 <romagnolimauricio@gmail.com>
2021-04-02 16:21:40 -0300

    feat: finish routes file

--- packages/server/src/routes.js
+++ packages/server/src/routes.js
@@ -1,4 +1,6 @@
 const url = require("url");
+const UploadHandler = require("./upload-handler");
+const { pipelineAsync, logger } = require('./util');
 
 class Routes {
   #io
@@ -6,13 +8,22 @@ class Routes {
     this.#io = io;
   }
 
+  async options(request, response) {
+    response.writeHead(204, {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS, POST'
+    });
+    response.end();
+  }
+
   async post(request, response) {
     const { headers } = request;
     const { query: { socketId } } = url.parse(request.url, true);
     const redirectTo = headers.origin;
 
-    this.#io.to(socketId).emit('file-uploaded', 5e9);
-    const onFinish = (response, redirectTo) => {
+    const uploadHandler = new UploadHandler(this.#io, socketId);
+
+    const onFinish = (response, redirectTo) => () => {
       response.writeHead(303, {
         Connection: 'close',
         Location: `${redirectTo}?msg=Files uploaded with success!`
@@ -21,9 +32,20 @@ class Routes {
       response.end();
     }
 
-    setTimeout(() => {
-      return onFinish(response, headers.origin);
-    }, 2000);
+    const busboyInstance = uploadHandler
+      .registerEvents(
+        headers,
+        onFinish(response, redirectTo)
+      );
+
+
+    await pipelineAsync(
+      request,
+      busboyInstance
+    )
+
+    logger.info('Request finished with success!');
+
   }
 }
 
